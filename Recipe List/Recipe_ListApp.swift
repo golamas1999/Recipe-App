78846840ef0d539242cb67a59f7714ae3dc02537
Jason Nelken <golamas1999@gmail.com>
2022-10-01 18:31:28 -0500

    Initial Commit

---
+++ Recipe List/Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ListApp.swift
+//  Recipe List
+//
+//  Created by Jason Nelken on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
